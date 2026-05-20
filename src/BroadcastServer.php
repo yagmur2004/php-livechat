92eb15ac09979092ad7ee0ef8b9c3db3abc7d6fc
Yagmur2004 <yagmurdirekci@gmail.com>
2026-05-20 07:42:08 +0300

    feat: integrate database layer into WebSocket server - save messages and room members

--- src/BroadcastServer.php
+++ src/BroadcastServer.php
@@ -4,11 +4,19 @@ namespace App;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
+require_once __DIR__ . '/../database/Database.php';
+require_once __DIR__ . '/../database/repositories/MessageRepository.php';
+require_once __DIR__ . '/../database/repositories/RoomMemberRepository.php';
+
 class BroadcastServer implements MessageComponentInterface {
     protected $clients;
+    protected $msgRepo;
+    protected $memberRepo;
 
     public function __construct() {
-        $this->clients = new \SplObjectStorage;
+        $this->clients   = new \SplObjectStorage;
+        $this->msgRepo   = new \MessageRepository();
+        $this->memberRepo = new \RoomMemberRepository();
     }
 
     public function onOpen(ConnectionInterface $conn) {
@@ -17,7 +25,6 @@ class BroadcastServer implements MessageComponentInterface {
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
-        // JSON parse et
         $data = json_decode($msg, true);
 
         if (!$data || !isset($data['type'])) {
@@ -27,6 +34,41 @@ class BroadcastServer implements MessageComponentInterface {
 
         echo "Broadcast message from {$from->resourceId}: $msg\n";
 
+        switch ($data['type']) {
+
+            case 'message':
+                // Mesajı DB'ye kaydet
+                if (isset($data['room_id'], $data['user_id'], $data['text'])) {
+                    $this->msgRepo->saveMessage(
+                        (int) $data['room_id'],
+                        (int) $data['user_id'],
+                        $data['text']
+                    );
+                }
+                break;
+
+            case 'join':
+                // Odaya katılımı DB'ye kaydet
+                if (isset($data['room_id'], $data['user_id'])) {
+                    $this->memberRepo->joinRoom(
+                        (int) $data['room_id'],
+                        (int) $data['user_id']
+                    );
+                }
+                break;
+
+            case 'leave':
+                // Odadan ayrılmayı DB'ye kaydet
+                if (isset($data['room_id'], $data['user_id'])) {
+                    $this->memberRepo->leaveRoom(
+                        (int) $data['room_id'],
+                        (int) $data['user_id']
+                    );
+                }
+                break;
+        }
+
+        // Herkese ilet
         foreach ($this->clients as $client) {
             $client->send($msg);
         }
